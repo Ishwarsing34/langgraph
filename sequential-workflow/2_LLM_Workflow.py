bb2568cd36bfcc7d23164db4fb1206d67cc305a8
Ishwarsing34 <igirase65@gmail.com>
2026-09-24 14:21:58 +0530

    #

---
+++ sequential-workflow/2_LLM_Workflow.py
@@ -0,0 +1,60 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from typing import TypedDict
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+model = ChatOpenAI()
+
+# create a state
+class LLMState(TypedDict):
+
+    question: str
+    answer: str
+
+
+
+def llm_qa(state: LLMState) -> LLMState:
+
+    # extract the question from state
+    question = state['question']
+
+    # form a prompt
+    prompt = f'Answer the following question {question}'
+
+    # ask that question to the LLM
+    answer = model.invoke(prompt).content
+
+    # update the answer in the state
+    state['answer'] = answer
+
+    return state
+
+
+
+
+# create our graph
+
+graph = StateGraph(LLMState)
+
+# add nodes
+graph.add_node('llm_qa', llm_qa)
+
+# add edges
+graph.add_edge(START, 'llm_qa')
+graph.add_edge('llm_qa', END)
+
+# compile
+workflow = graph.compile()
+
+
+# execute
+
+intial_state = {'question': 'Who is the creator of Python?'}
+
+final_state = workflow.invoke(intial_state)
+
+print(final_state['answer'])
